629d800920280a87012b290be1a910cd25fd6ab5
fretchen <fred.jendrzejewski@gmail.com>
2024-01-29 21:49:15 +0100

    Set up the main pages.

---
+++ qlued-next/src/app/devices/page.tsx
@@ -0,0 +1,11 @@
+
+import Typography from '@mui/material/Typography';
+export default function Devices() {
+  return (
+    <main >
+      <div >
+        <Typography>I am the device page.</Typography> 
+      </div>
+    </main>
+  );
+}
---
+++ qlued-next/src/app/user/page.tsx
@@ -0,0 +1,11 @@
+
+import Typography from '@mui/material/Typography';
+export default function Devices() {
+  return (
+    <main >
+      <div >
+        <Typography>I am the user page.</Typography> 
+      </div>
+    </main>
+  );
+}
